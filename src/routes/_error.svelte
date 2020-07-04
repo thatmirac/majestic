df0ed45c6fa583bb62e7ecebda6d11fb5d14504f
Renaitare <git@renai.me>
2020-07-04 11:40:45 -0400

    Optimized Boilerplate Code

---
+++ src/routes/_error.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let status;
+	export let error;
+
+	const dev = process.env.NODE_ENV === 'development';
+</script>
+
+<svelte:head>
+	<title>{status}</title>
+</svelte:head>
+
+<div class="showcase">
+	<span>
+		<h1>{status}</h1>
+		<h3 class="stack">{error.message}</h3>
+		{#if dev && error.stack}
+			<pre class="stack" style="margin-top: 48px;">{error.stack}</pre>
+		{/if}
+	</span>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { onMount } from 'svelte';
+	import { goto } from '@sapper/app';
+	onMount(() => {
+		goto('wallpapers/');
+	});
+</script>
+
+<svelte:head>
+	<!-- Primary Meta Tags -->
+	<title>Made by Mirac</title>
+	<link rel="canonical" href="https://madebymirac.com/" />
+	<meta name="title" content="Made by Mirac" />
+	<meta
+		name="description"
+		content="Home of original content that has been thoughtfully planned and
+		designed by Mirac." />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://madebymirac.com/" />
+	<meta property="og:title" content="Made by Mirac" />
+	<meta
+		property="og:description"
+		content="Home of original content that has been thoughtfully planned and
+		designed by Mirac." />
+	<meta
+		property="og:image"
+		content="https://madebymirac.com/assets/card.png" />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://madebymirac.com/" />
+	<meta property="twitter:title" content="Made by Mirac" />
+	<meta name="twitter:creator" content="@thatmirac" />
+	<meta name="twitter:creator:id" content="1153786059718889472" />
+	<meta
+		property="twitter:description"
+		content="Home of original content that has been thoughtfully planned and
+		designed by Mirac." />
+	<meta
+		property="twitter:image"
+		content="https://madebymirac.com/assets/card.png" />
+	<meta name="twitter:image:alt" content="Mirac Logo Card" />
+</svelte:head>
