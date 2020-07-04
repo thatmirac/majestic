9556fc8f4c5d7dddeddc02c51f529def0d7d53f7
Renaitare <git@renai.me>
2020-07-04 12:13:16 -0400

    Apparently Sirv breaks without this

--- src/server.js
+++ src/server.js
@@ -7,7 +7,11 @@ const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka()
-	.use(compression(), sirv('static', { dev }), sapper.middleware())
+	.use(
+		compression({ threshold: 0 }),
+		sirv('static', { dev }),
+		sapper.middleware()
+	)
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
 	});
