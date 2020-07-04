12d9c5c3e1fb2282f0c07b83a68c9e7c9adc2b75
Renaitare <git@renai.me>
2020-07-04 06:05:31 -0400

    Sapper + Static Assets

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let segment;
+</script>
+
+<header>
+	<div class="logo-container">
+		<a href=".">
+			<img src="assets/header.png" alt="Home" />
+		</a>
+	</div>
+</header>
+
+<main>
+	<slot {segment} />
+</main>
