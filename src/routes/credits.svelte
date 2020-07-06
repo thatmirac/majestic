de2542ea1dcbc0dc44b690f4bc5248916cfbfab6
Renaitare <git@renai.me>
2020-07-06 16:58:39 -0400

    Credits Page

---
+++ src/routes/credits.svelte
@@ -0,0 +1,81 @@
+<main>
+	<div class="wallprev-container" id="creditspage">
+		<div class="credits-title">
+			<h1>CREDITS</h1>
+		</div>
+		<div class="credits-credits-section">
+			<img src="assets/profiles/renai.png" alt="profile" />
+			<div class="credits-text">
+				<p>B A C K E N D &nbsp; D E V E L O P M E N T</p>
+				<h1>Renaitare</h1>
+				<div class="social-links">
+					<a href="mailto:support@renai.me">
+						<img src="assets/icons/mail.png" alt="mail" />
+					</a>
+					<a href="https://twitter.com/Renaitare">
+						<img src="assets/icons/twitter.png" alt="twitter" />
+					</a>
+					<a href="https://github.com/Renaitare">
+						<img src="assets/icons/github.png" alt="github" />
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="credits-credits-section">
+			<img src="assets/profiles/mirac.png" alt="profile" />
+			<div class="credits-text">
+				<p>F R O N T E N D &nbsp; D E V E L O P M E N T</p>
+				<h1>Mirac</h1>
+				<div class="social-links">
+					<a href="mailto:ozd.mirac@gmail.com">
+						<img src="assets/icons/mail.png" alt="mail" />
+					</a>
+					<a href="https://twitter.com/thatmirac">
+						<img src="assets/icons/twitter.png" alt="twitter" />
+					</a>
+					<a href="https://www.reddit.com/user/MiracoZ">
+						<img src="assets/icons/reddit.png" alt="reddit" />
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+
+<svelte:head>
+	<!-- Primary Meta Tags -->
+	<title>Made by Mirac | Credits</title>
+	<link rel="canonical" href="https://madebymirac.com/credits" />
+	<meta name="title" content="Made by Mirac | Credits" />
+	<meta
+		name="description"
+		content="Home of original content that has been thoughtfully planned and
+		designed by Mirac." />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://madebymirac.com/credits" />
+	<meta property="og:title" content="Made by Mirac | Credits" />
+	<meta
+		property="og:description"
+		content="Home of original content that has been thoughtfully planned and
+		designed by Mirac." />
+	<meta
+		property="og:image"
+		content="https://madebymirac.com/assets/card.jpg" />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://madebymirac.com/credits" />
+	<meta property="twitter:title" content="Made by Mirac | Credits" />
+	<meta name="twitter:creator" content="@thatmirac" />
+	<meta name="twitter:creator:id" content="1153786059718889472" />
+	<meta
+		property="twitter:description"
+		content="Home of original content that has been thoughtfully planned and
+		designed by Mirac." />
+	<meta
+		property="twitter:image"
+		content="https://madebymirac.com/assets/card.jpg" />
+	<meta name="twitter:image:alt" content="Mirac Logo Card" />
+</svelte:head>
