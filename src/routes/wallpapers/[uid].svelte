0c37b5a4a8e65d7a8dcf2804dcc026308d89955e
Renaitare <git@renai.me>
2020-07-07 05:46:14 -0400

    Remove duplicated body on wallpaper preview page

--- src/routes/wallpapers/[uid].svelte
+++ src/routes/wallpapers/[uid].svelte
@@ -82,140 +82,138 @@
 	export let uid;
 </script>
 
-<body>
-	<div class="wallprev-container">
-		<div class="top-image">
-			<div class="top-image-background">
-				<img src={data.header} alt={`${data.name} Header`} />
-			</div>
-			<div class="top-image-overlay" />
-			<div class="top-image-elements">
-				<div class="title">
-					<h1>{data.date}</h1>
-					<h2>{data.name}</h2>
-				</div>
-				<div class="get-button-container">
-					<div class="get-button">
-						<a
-							href="index.html"
-							style="color: rgba(255, 255, 255, 0.9);">
-							GET
-						</a>
-					</div>
-				</div>
-			</div>
+<div class="wallprev-container">
+	<div class="top-image">
+		<div class="top-image-background">
+			<img src={data.header} alt={`${data.name} Header`} />
 		</div>
-		{#if data.credits}
-			<div class="credits-section">
-				<p>{data.credits.primary.text[0].text}</p>
-				<div class="user">
-					<img
-						src={data.credits.primary.image.url}
-						alt={data.credits.primary.author[0].text} />
-					<a href={data.credits.primary.link.url}>
-						{data.credits.primary.author[0].text}
+		<div class="top-image-overlay" />
+		<div class="top-image-elements">
+			<div class="title">
+				<h1>{data.date}</h1>
+				<h2>{data.name}</h2>
+			</div>
+			<div class="get-button-container">
+				<div class="get-button">
+					<a
+						href="index.html"
+						style="color: rgba(255, 255, 255, 0.9);">
+						GET
 					</a>
 				</div>
 			</div>
-			<div class="seperator" />
-		{/if}
-		<div class="screenshots-section">
-			<div class="subtitle" id="screenshots-title">
-				<h1>Preview</h1>
+		</div>
+	</div>
+	{#if data.credits}
+		<div class="credits-section">
+			<p>{data.credits.primary.text[0].text}</p>
+			<div class="user">
+				<img
+					src={data.credits.primary.image.url}
+					alt={data.credits.primary.author[0].text} />
+				<a href={data.credits.primary.link.url}>
+					{data.credits.primary.author[0].text}
+				</a>
 			</div>
-			<div class="screenshots-wrapper">
-				{#each data.screenshots as shot}
-					<img src={shot.image.url} alt={`${data.name} Image`} />
+		</div>
+		<div class="seperator" />
+	{/if}
+	<div class="screenshots-section">
+		<div class="subtitle" id="screenshots-title">
+			<h1>Preview</h1>
+		</div>
+		<div class="screenshots-wrapper">
+			{#each data.screenshots as shot}
+				<img src={shot.image.url} alt={`${data.name} Image`} />
+			{/each}
+			<div class="spacer" />
+		</div>
+	</div>
+	<div class="seperator" />
+	<div class="sizes-section">
+		<div class="subtitle">
+			<h1>Sizes</h1>
+		</div>
+		<div class="download-grid">
+			{#if data.mobile}
+				<div class="download-container-phone">
+					<div class="phone">
+						<div class="phone-shape" />
+						<div class="phone-home-button" />
+					</div>
+					<div class="resolution">1125 × 2436</div>
+					<div class="download-button">
+						<a
+							href={data.mobile.primary.image.url.slice(0, data.mobile.primary.image.url.indexOf('?auto=compress'))}
+							style="color: rgba(255, 255, 255, 1);"
+							download>
+							<p>Download</p>
+						</a>
+					</div>
+				</div>
+			{/if}
+			{#if data.tablet}
+				<div class="download-container-tablet">
+					<div class="tablet">
+						<div class="tablet-shape" />
+						<div class="tablet-home-button" />
+					</div>
+					<div class="resolution">2388 × 1668</div>
+					<div class="download-button">
+						<a
+							href={data.tablet.primary.image.url.slice(0, data.tablet.primary.image.url.indexOf('?auto=compress'))}
+							style="color: rgba(255, 255, 255, 1);"
+							download>
+							<p>Download</p>
+						</a>
+					</div>
+				</div>
+			{/if}
+			{#if data.desktop}
+				<div class="download-container-computer">
+					<div class="computer">
+						<div class="computer-shape" />
+						<div class="computer-stand" />
+					</div>
+					<div class="resolution">2560 × 1440</div>
+					<div class="download-button" download>
+						<a
+							href={data.desktop.primary.image.url.slice(0, data.desktop.primary.image.url.indexOf('?auto=compress'))}
+							style="color: rgba(255, 255, 255, 1);"
+							download>
+							<p>Download</p>
+						</a>
+					</div>
+				</div>
+			{/if}
+		</div>
+	</div>
+	<div class="seperator" />
+	{#if data.related.length > 0}
+		<div class="related-section">
+			<div class="subtitle" id="related-title">
+				<h1>More Like This</h1>
+			</div>
+			<div class="related-wrapper">
+				{#each data.related as wallpaper}
+					<a href={`/wallpapers/${wallpaper.uid}`}>
+						<img
+							src={wallpaper.preview}
+							alt={`${wallpaper.name} Image`} />
+					</a>
 				{/each}
 				<div class="spacer" />
 			</div>
 		</div>
 		<div class="seperator" />
-		<div class="sizes-section">
-			<div class="subtitle">
-				<h1>Sizes</h1>
-			</div>
-			<div class="download-grid">
-				{#if data.mobile}
-					<div class="download-container-phone">
-						<div class="phone">
-							<div class="phone-shape" />
-							<div class="phone-home-button" />
-						</div>
-						<div class="resolution">1125 × 2436</div>
-						<div class="download-button">
-							<a
-								href={data.mobile.primary.image.url.slice(0, data.mobile.primary.image.url.indexOf('?auto=compress'))}
-								style="color: rgba(255, 255, 255, 1);"
-								download>
-								<p>Download</p>
-							</a>
-						</div>
-					</div>
-				{/if}
-				{#if data.tablet}
-					<div class="download-container-tablet">
-						<div class="tablet">
-							<div class="tablet-shape" />
-							<div class="tablet-home-button" />
-						</div>
-						<div class="resolution">2388 × 1668</div>
-						<div class="download-button">
-							<a
-								href={data.tablet.primary.image.url.slice(0, data.tablet.primary.image.url.indexOf('?auto=compress'))}
-								style="color: rgba(255, 255, 255, 1);"
-								download>
-								<p>Download</p>
-							</a>
-						</div>
-					</div>
-				{/if}
-				{#if data.desktop}
-					<div class="download-container-computer">
-						<div class="computer">
-							<div class="computer-shape" />
-							<div class="computer-stand" />
-						</div>
-						<div class="resolution">2560 × 1440</div>
-						<div class="download-button" download>
-							<a
-								href={data.desktop.primary.image.url.slice(0, data.desktop.primary.image.url.indexOf('?auto=compress'))}
-								style="color: rgba(255, 255, 255, 1);"
-								download>
-								<p>Download</p>
-							</a>
-						</div>
-					</div>
-				{/if}
-			</div>
-		</div>
-		<div class="seperator" />
-		{#if data.related.length > 0}
-			<div class="related-section">
-				<div class="subtitle" id="related-title">
-					<h1>More Like This</h1>
-				</div>
-				<div class="related-wrapper">
-					{#each data.related as wallpaper}
-						<a href={`/wallpapers/${wallpaper.uid}`}>
-							<img
-								src={wallpaper.preview}
-								alt={`${wallpaper.name} Image`} />
-						</a>
-					{/each}
-					<div class="spacer" />
-				</div>
-			</div>
-			<div class="seperator" />
-		{/if}
-		<div class="footer" id="wppagefooter">
-			<p>©{new Date().getFullYear()}, Mirac</p>
-			<a href="/credits">
-				<p>Credits</p>
-			</a>
-		</div>
+	{/if}
+	<div class="footer" id="wppagefooter">
+		<p>©{new Date().getFullYear()}, Mirac</p>
+		<a href="/credits">
+			<p>Credits</p>
+		</a>
 	</div>
-</body>
+</div>
 
 <svelte:head>
 	<!-- Primary Meta Tags -->
