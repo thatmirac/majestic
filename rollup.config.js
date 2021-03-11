624c99455da975bfb3e0f86cf8d45bc6f59addb1
Aarnav Tale <aarnavxtale@gmail.com>
2021-03-11 11:07:44 -0500

    [ts] update core project files

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,14 @@
+import path from 'path';
 import alias from '@rollup/plugin-alias';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
+import url from '@rollup/plugin-url';
 import svelte from 'rollup-plugin-svelte';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+import sveltePreprocess from 'svelte-preprocess';
+import typescript from '@rollup/plugin-typescript';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
 
@@ -13,66 +17,68 @@ const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
-	(warning.code === 'CIRCULAR_DEPENDENCY' &&
-		/[/\\]@sapper[/\\]/.test(warning.message)) ||
-	onwarn(warning);
+	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
+	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
+	(warning.code === 'THIS_IS_UNDEFINED') ||
+	(warning.plugin === 'css' && warning.message === 'No directory provided. Skipping CSS generation') ||
+	onwarn(warning)
 
 export default {
 	client: {
-		input: config.client.input(),
+		input: config.client.input().replace(/\.js$/, '.ts'),
 		output: config.client.output(),
 		plugins: [
 			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode),
+				preventAssignment: true,
+				values: {
+					'process.browser': true,
+					'process.env.NODE_ENV': JSON.stringify(mode)
+				},
 			}),
 			svelte({
-				dev,
-				hydratable: true,
-				emitCss: true,
+				preprocess: sveltePreprocess({ sourceMap: dev }),
+				compilerOptions: {
+					dev,
+					hydratable: true
+				}
 			}),
 			alias({
-				entries: [
-					{
-						find: `@sapper`,
-						replacement: `${__dirname}/src/node_modules/@sapper`,
-					},
-				],
+				entries: [{
+					find: `@sapper`,
+					replacement: `${__dirname}/src/node_modules/@sapper`
+				}]
+			}),
+			url({
+				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
+				publicPath: '/client/'
 			}),
 			resolve({
 				browser: true,
-				dedupe: ['svelte'],
+				dedupe: ['svelte']
 			}),
 			commonjs(),
+			typescript({ sourceMap: dev }),
 
-			legacy &&
-				babel({
-					extensions: ['.js', '.mjs', '.html', '.svelte'],
-					babelHelpers: 'runtime',
-					exclude: ['node_modules/@babel/**'],
-					presets: [
-						[
-							'@babel/preset-env',
-							{
-								targets: '> 0.25%, not dead',
-							},
-						],
-					],
-					plugins: [
-						'@babel/plugin-syntax-dynamic-import',
-						[
-							'@babel/plugin-transform-runtime',
-							{
-								useESModules: true,
-							},
-						],
-					],
-				}),
+			legacy && babel({
+				extensions: ['.js', '.mjs', '.html', '.svelte'],
+				babelHelpers: 'runtime',
+				exclude: ['node_modules/@babel/**'],
+				presets: [
+					['@babel/preset-env', {
+						targets: '> 0.25%, not dead'
+					}]
+				],
+				plugins: [
+					'@babel/plugin-syntax-dynamic-import',
+					['@babel/plugin-transform-runtime', {
+						useESModules: true
+					}]
+				]
+			}),
 
-			!dev &&
-				terser({
-					module: true,
-				}),
+			!dev && terser({
+				module: true
+			})
 		],
 
 		preserveEntrySignatures: false,
@@ -80,61 +86,70 @@ export default {
 	},
 
 	server: {
-		input: config.server.input(),
+		input: { server: config.server.input().server.replace(/\.js$/, ".ts") },
 		output: config.server.output(),
 		plugins: [
 			replace({
-				'process.browser': false,
-				'process.env.NODE_ENV': JSON.stringify(mode),
+				preventAssignment: true,
+				values: {
+					'process.browser': false,
+					'process.env.NODE_ENV': JSON.stringify(mode)
+				},
 			}),
 			svelte({
-				generate: 'ssr',
-				dev,
+				preprocess: sveltePreprocess({ sourceMap: dev }),
+				compilerOptions: {
+					dev,
+					generate: 'ssr',
+					hydratable: true
+				},
+				emitCss: false
+			}),
+			url({
+				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
+				publicPath: '/client/',
+				emitFiles: false // already emitted by client build
 			}),
 			alias({
-				entries: [
-					{
-						find: `@sapper`,
-						replacement: `${__dirname}/src/node_modules/@sapper`,
-					},
-				],
+				entries: [{
+					find: `@sapper`,
+					replacement: `${__dirname}/src/node_modules/@sapper`
+				}]
 			}),
 			resolve({
-				dedupe: ['svelte'],
+				dedupe: ['svelte']
 			}),
 			commonjs(),
+			typescript({ sourceMap: dev })
 		],
-		external: Object.keys(pkg.dependencies).concat(
-			require('module').builtinModules ||
-				Object.keys(process.binding('natives'))
-		),
-
+		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
 		preserveEntrySignatures: 'strict',
 		onwarn,
 	},
 
 	serviceworker: {
-		input: config.serviceworker.input(),
+		input: config.serviceworker.input().replace(/\.js$/, '.ts'),
 		output: config.serviceworker.output(),
 		plugins: [
 			resolve(),
 			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode),
+				preventAssignment: true,
+				values: {
+					'process.browser': true,
+					'process.env.NODE_ENV': JSON.stringify(mode)
+				},
 			}),
 			alias({
-				entries: [
-					{
-						find: `@sapper`,
-						replacement: `${__dirname}/src/node_modules/@sapper`,
-					},
-				],
+				entries: [{
+					find: `@sapper`,
+					replacement: `${__dirname}/src/node_modules/@sapper`
+				}]
 			}),
 			commonjs(),
-			!dev && terser(),
+			typescript({ sourceMap: dev }),
+			!dev && terser()
 		],
-
 		preserveEntrySignatures: false,
 		onwarn,
-	},
+	}
 };
