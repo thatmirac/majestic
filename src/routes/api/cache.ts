70cfb90714964f49256222d330ed5ac43c772e28
Aarnav Tale <aarnavxtale@gmail.com>
2021-04-19 13:16:07 -0400

    [api] support cache clearing for cloudflare

---
+++ src/routes/api/cache.ts
@@ -0,0 +1,38 @@
+import fetch from 'node-fetch';
+
+export async function get(req, res, next) {
+	const result = await fetch(`https://api.cloudflare.com/client/v4/zones/${process.env.CLOUDFLARE_ZONE}/purge_cache`, {
+		method: 'POST',
+		headers: {
+			'Authorization': `Bearer ${process.env.CLOUDFLARE_BEARER}`,
+			'Content-Type': 'application/json; charset=utf-8',
+			'User-Agent': 'Majestic/1.0 (+https://tale.me/go/ua#majestic)'
+		},
+		body: JSON.stringify({
+			purge_everything: true
+		})
+	})
+
+	if (result.ok) {
+		const data = await result.json()
+		if (data.success === true) {
+			res
+				.writeHead(200, {
+					'Content-Type': 'application/json'
+				})
+				.end(JSON.stringify({
+					success: true
+				}))
+			return
+		}
+	}
+
+	res
+		.writeHead(200, {
+			'Content-Type': 'application/json'
+		})
+		.end(JSON.stringify({
+			success: false
+		}))
+	return
+}
